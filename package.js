23fe997cab4e11cda50119657d16c028ccb71c44
Daniel Li <dan@danyll.com>
2015-09-07 22:49:23 +0800

    Using cosmos:browserify 0.5.0

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'd4nyll:moment',
-  version: '0.0.5',
+  version: '0.0.6',
   // Brief, one-line summary of the package.
   summary: 'Meteor wrapper for Moment and Moment Timezone',
   // URL to the Git repository containing the source code for this package.
@@ -17,7 +17,7 @@ Npm.depends({
 
 Package.onUse(function(api) {
   api.versionsFrom('1.1.0.3');
-  api.use(['cosmos:browserify@0.2.0'], 'client');
+  api.use(['cosmos:browserify@0.5.0'], 'client');
   api.addFiles('moment.browserify.js', 'client');
   api.export('moment', 'client');
 });
